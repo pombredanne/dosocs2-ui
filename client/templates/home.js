530bea8da359c7864f4943f79330dd71d41da623
Sai Uday <uday@socs.ist.unomaha.edu>
2016-04-13 09:34:14 -0500

    Login redirect - user must be logged in for using DOSOCS2-UI

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,6 +1,7 @@
 
 Template['home'].helpers({
   spdxdoc: function(){
+    console.log(Session.get("spdxdoc"));
     return Session.get("spdxdoc");
   }
 });
@@ -20,7 +21,7 @@ Template['uploadedInfo'].events({
   },
   'click .SPDXGenerate':function(event, template) {
     event.preventDefault();
-    sAlert.info("SPDX Generation in progress", {effect: 'bouncyflip', position: 'top-right', timeout: 3000, onRouteClose: true, stack: true, offset: '100px'});
+    sAlert.info("SPDX Generation in progress", {effect: 'bouncyflip', position: 'top-right', onRouteClose: true, stack: true, offset: '100px'});
     Meteor.call('generateSPDX', this._id, function(err, response){
         if(err){
           sAlert.error("Error in scanning " + this._id, {effect: 'bouncyflip', position: 'top-right', timeout: 3000, onRouteClose: true, stack: true, offset: '100px'});
